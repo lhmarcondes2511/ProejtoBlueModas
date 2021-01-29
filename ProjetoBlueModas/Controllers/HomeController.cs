841ee68ea400d7dad3105f259dfc51d88fa868df
Lucas Henrique <lucashenrique125@hotmail.com>
2021-01-29 14:58:28 -0400

    Projeto Criado

---
+++ ProjetoBlueModas/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjetoBlueModas.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoBlueModas.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        /* 
+            Páginas
+        */
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Produto() {
+            return View();
+        }
+
+        public IActionResult Sobre() {
+            return View();
+        }
+        /* 
+            Fim Páginas
+        */
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProjetoBlueModas/Models/DbContext/BlueModasContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoBlueModas.Models {
+    public class BlueModasContext : DbContext {
+        public BlueModasContext(DbContextOptions<BlueModasContext> options) : base(options) {}
+
+    }
+}
